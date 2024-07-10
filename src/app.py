7410ab2f50605cfea91fd851c1cb1d05818aa31b
Quang Huy <huytq@Ubuntu22.04>
2024-07-10 14:13:25 +0700

    update code to get Environment from ConfigMap and Secrets

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, Response
+import os
 
 app = Flask(__name__)
 
@@ -6,7 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return Response(
-        "Hello, World! \nThis is Huy's webapi",
+        "Hello, World! "
+        "\nThis is Huy's webapi "
+        "\nConfigMap: NAME: {0}, EMAIL: {1}"
+        "\nSecret: USER: {2}, PASS: {3}".format(os.getenv("NAME"), os.getenv("EMAIL"), os.getenv("USER"), os.getenv("PASS")),
         mimetype='text/plain'
     )
 
